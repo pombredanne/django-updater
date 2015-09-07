77c3392aa0319ba674807bd975275b2302fe09d4
Jay <ja.geb@me.com>
2015-09-07 16:01:47 +0200

    added (optional) UPDATER_TOKEN setting

--- updater/conf.py
+++ updater/conf.py
@@ -13,5 +13,6 @@ class Settings(BaseSettings):
     UPDATER_DAYS_BETWEEN_NOTIFICATION = getattr(django_settings, "UPDATER_DAYS_BETWEEN_NOTIFICATION", 7)
     UPDATER_NOTIFY_ON_UPDATES = getattr(django_settings, "UPDATER_NOTIFY_ON_UPDATES", True)
     UPDATER_EMAILS = getattr(django_settings, "UPDATER_EMAILS", [mail for name, mail in django_settings.ADMINS])
+    UPDATER_TOKEN = getattr(django_settings, "UPDATER_TOKEN", False)
 
 settings = Settings()
