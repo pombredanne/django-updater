0d2d6d8a87991a8c4d74a2f5b9d421f26e924c90
Jay <ja.geb@me.com>
2015-09-09 16:28:46 +0200

    removed unused import

--- updater/urls.py
+++ updater/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .views import run_view
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 urlpatterns = [
     url(r'run/(?P<token>[0-9a-f-]{36})/$', run_view, name="updater_run"),
