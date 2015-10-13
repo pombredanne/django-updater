ceb5fb7005d61cc0247941de917ae61eb552bbda
Jay <ja.geb@me.com>
2015-10-13 14:18:18 +0200

    squashed migrations

--- updater/migrations/0001_initial.py
+++ updater/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import uuid
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
@@ -12,10 +13,23 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Token',
+            name='Notification',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('token', models.CharField(default=uuid.uuid4, max_length=36)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('created', models.DateTimeField(default=django.utils.timezone.now)),
+                ('security_issue', models.BooleanField(default=False)),
             ],
+        ),
+        migrations.CreateModel(
+            name='Status',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('registered', models.BooleanField(default=False)),
+                ('site_token', models.CharField(max_length=36, default=uuid.uuid4)),
+            ],
+            options={
+                'verbose_name': 'Status',
+                'verbose_name_plural': 'Status',
+            },
         ),
     ]
